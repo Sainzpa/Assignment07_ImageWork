6b61e8651ded0c37e4ee589e2b707b6bc988cd96
pandorasainz <pandorasainz@192.168.10.6>
2021-07-14 17:08:48 +0500

    follow the lecture

--- Sainzpa_Assignment07/Sainzpa_Assignment07/Sainzpa_Assignment07.py
+++ Sainzpa_Assignment07/Sainzpa_Assignment07/Sainzpa_Assignment07.py
@@ -2,7 +2,52 @@
 Created on 13 Jul 2021
 
 @author: pandorasainz
+citations: 
+1- https://stackoverflow.com/questions/8863917/importerror-no-module-named-pil #to import pillow
+2- https://stackoverflow.com/questions/67209502/import-requests-modulenotfounderror-no-module-named-requests # to import requests
 '''
+from PIL import Image, ImageFilter, ImageDraw, ImageFont
+import os, sys
+import requests
+from io import BytesIO
+from image_functions import *
+
+
 
 if __name__ == '__main__':
-    pass
+    # open an image file. The default path is where this python module is
+    '''
+    im = Image.open("SiriusAndViolet.jpg")
+    print(im.width, im.height, im.mode, im.format)  # Display some info about the image
+    '''
+    
+    # Load and Show Image
+    '''
+    my_image = load_image("SiriusAndViolet.jpg")
+    if my_image != None:
+        my_image.show(my_image)
+    else: 
+        print('unable to open the file.')
+    '''
+        
+    #crop Image
+    '''
+    im = Image.open("SiriusAndViolet.jpg")  
+    im_c = im.crop((200,300,400,500)) # (left, top, right, bottom) it's a tuple!
+    im_c.show()
+    '''
+    
+    #blur image
+    '''
+    my_image = blur_image(load_image("SiriusAndViolet.jpg"))
+    my_image.show()
+    '''
+    
+    
+    
+    
+    
+    
+    
+    
+    
--- Sainzpa_Assignment07/Sainzpa_Assignment07/image_functions.py
+++ Sainzpa_Assignment07/Sainzpa_Assignment07/image_functions.py
@@ -3,6 +3,48 @@ Created on 13 Jul 2021
 
 @author: pandorasainz
 '''
+from PIL import Image, ImageFilter, ImageDraw, ImageFont
+import os, sys
+import requests
+from io import BytesIO
 
-if __name__ == '__main__':
-    print("Test Commit")
+
+'''
+    Load an image file from disk
+    :param filename: The file to load
+    :return the image object
+'''
+def load_image(filename):
+    try: 
+        myimage = Image.open(filename)
+        myimage.load()
+        return myimage
+    except: 
+       return None
+
+
+def save_image(imageObject, outfilename) :
+    """
+    Save an image to disk
+    :param imageObject: The Image to save
+    :param outfilename: The target file
+    """
+    try: 
+        imageObject.save(outfilename)
+    except: 
+        return None
+    
+    
+def blur_image(imageObject):
+    # Doesn't look too different but it does blur a little. Depends on the original image
+    blurred = imageObject.filter(ImageFilter.BLUR)
+    # blurred = imageObject.filter(ImageFilter.EMBOSS)    # This produces dramatic results. 
+    # blurred = imageObject.filter(ImageFilter.DETAIL)    # Doesn't look much different 
+    return blurred
+
+
+
+
+    
+    
+
